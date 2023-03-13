b1fb542f9d08648261ca38a2ab68e56847fb37d4
Kenny K <koablood@gmail.com>
2023-03-13 14:46:37 -0700

    Using JWTs

---
+++ backend/migrations/20210726171949-create-comment.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('comments', {
+      comment_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      place_id: {
+        type: Sequelize.SMALLINT
+      },
+      stars: {
+        type: Sequelize.FLOAT
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      rant: {
+        type: Sequelize.BOOLEAN
+      },
+      author_id: {
+        type: Sequelize.SMALLINT
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('comments');
+  }
+};
